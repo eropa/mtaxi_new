ad3a72487d4b490c87f656eeb35ea7b0219c6c35
Igor Lutsenko <eropamd@gmail.com>
2020-11-30 11:28:42 +0200

    fix YEAR

--- app/Services/PrintService.php
+++ app/Services/PrintService.php
@@ -829,7 +829,7 @@ class PrintService{
             }else{
                 $pdf->Write(0,iconv('utf-8',
                     'windows-1251',
-                    "**21"));
+                    $arrayData[$i]['m2']."**21"));
             }
 
 
